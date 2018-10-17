9d681d897977d3504c1114e402b83c7f6445dbe5
Melih Yumak <melih193@gmail.com>
2018-10-17 13:35:08 +0200

    Storybook addons added

---
+++ .storybook/WelcomeStory.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Welcome', module)
+.add('to your storybook 🐶',
+() => <h1> welcome to your storybook</h1>
+)
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,13 @@
-import {configure} from '@storybook/react'
+import {configure, setAddon} from '@storybook/react';
+import JSXAddon from 'storybook-addon-jsx';
+
+
+setAddon(JSXAddon);
 
 const req = require.context('../src', true , /.stories.js$/)
 
 function loadStories(){
+    require('./WelcomeStory')
     req.keys().forEach(file => {
         req(file)
     });
