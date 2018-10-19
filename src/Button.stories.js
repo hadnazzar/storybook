40636067d7ba7b1fdda9f7d0034921408f53f894
Melih Yumak <melih193@gmail.com>
2018-10-19 14:58:14 +0200

    storybook completed

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { wInfo } from './utils';
 
-import {storiesOf} from '@storybook/react'
-import {Button} from './Button'
+import { storiesOf } from '@storybook/react';
+import { Button } from './Button';
+import { text, boolean } from '@storybook/addon-knobs/react';
 
-storiesOf('Button',module)
-.addWithJSX('with background', () => ( 
-    <Button bg='gray'>Hello World</Button>
-))
-.addWithJSX('with green background', () => ( 
-    <Button bg='green'>Hello World</Button>
-))
+storiesOf('Button', module)
+  .addWithJSX(
+    'with background',
+    wInfo(`
+      description
+      
+      ~~~js
+      <Button>slkdjslkdj</Button>
+      ~~~
+      `)(() => <Button bg="palegoldenrod">Hello world</Button>),
+  )
+  .addWithJSX('with background 2', () => (
+    <Button disabled={boolean('disabled', false)}>
+      {text('text', 'Hello world')}
+    </Button>
+  ));
